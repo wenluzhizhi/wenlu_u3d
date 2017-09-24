be536a44bcd5ebcb1e4d0092051b6b3ad0bc6e59
wenluzhizhi <fang10159066099@163.com>
2017-09-24 18:10:53 +0800

    add fog and shadow asset

---
+++ fog_shadow/Assets/Scripts/CameraRender.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRender : MonoBehaviour {
+
+
+	public Material ma;
+	void Start () {
+		Camera.main.depthTextureMode = DepthTextureMode.Depth;
+	}
+
+
+	void Update () {
+
+	}
+
+
+	//[ImageEffectOpaque]
+	void OnRenderImage(RenderTexture src,RenderTexture dest){
+		Graphics.Blit (src, dest, ma);
+	}
+
+	public void OnClickSliderChange(float value){
+		ma.SetFloat ("_Gloss",value);
+	}
+}
